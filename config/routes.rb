c4e6c7b18a96613c7431ca775d6db9f5fa1b897a
EC2 Default User <111130918+roherstein@users.noreply.github.com>
2022-10-25 13:56:32 +0000

    [finish]4-18

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show,:edit]
+  #get 'users/show'
+  #get 'users/edit'
+  resources :bookers, only: [:new,:index,:show,:edit]
+  #get 'books/new'
+  #get 'books/index'
+  #get 'books/show'
+  #get 'books/edit'
+  get 'homes/top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
