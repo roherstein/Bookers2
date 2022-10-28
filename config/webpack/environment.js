1bec097f7ddd85d3a626cabf509983379f7c7eb4
EC2 Default User <111130918+roherstein@users.noreply.github.com>
2022-10-28 14:02:54 +0000

    [finish]4-19

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,9 +4,10 @@ module.exports = environment
 
 const webpack = require('webpack')
 environment.plugins.prepend(
-  'Provide'
+  'Provide',
   new webpack.ProvidePlugin({
      $:'jquery/src/jquery',
+     jQuery:'jquery/src/jquery',
      Popper:'popper.js'
   })
 )
