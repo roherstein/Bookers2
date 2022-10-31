2b9f544dfb31c70340b7a2ab2f17bc6b0d763a53
EC2 Default User <111130918+roherstein@users.noreply.github.com>
2022-10-31 15:11:30 +0000

    [finish]4-26

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    @post_images = @user.post_images
-  end
 
   def edit
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_21_142159) do
+ActiveRecord::Schema.define(version: 2022_10_29_090032) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "user_id"
   end
 
   create_table "users", force: :cascade do |t|
